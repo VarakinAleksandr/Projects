e39346b2f00fe1d50ecbb92747d60beb4ce3b6e8
VarakinAleksandr <sanek-varakin@mail.ru>
2024-11-27 10:47:58 +0400

    add Leson2 Task4

---
+++ 01_CSHARP/Lesson2/Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿int i = 0;
+int[] arr = { 5, 4, 7, 9, 5, 8, 22, 222, 1, 1, 2 };
+int max = arr[0];
+while (i < arr.Length)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    i++;
+}
+Console.WriteLine(max);
+
+foreach(int e in arr)
+{
+    if (e>max)
+    {
+        max=e;
+    }
+}
+Console.WriteLine(max);
