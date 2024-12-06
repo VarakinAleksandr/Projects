106af6f570d6200f4160543b38b1cc7c62f2efcd
AleksandrVarakin <alexander.varakin@gmail.com>
2024-12-06 14:36:52 +0400

    Add Lesson4 Task1

---
+++ 01_CSHARP/Lesson4/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+int [,] CrsateMatrix (int rowCount, int columeCount)
+{
+    int[,] matrix = new int[rowCount, columeCount];
+
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++) //  0  это строки
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // 1 столбцы
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+void ShowMatrix (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) //  0  это строки
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // 1 столбцы
+        {
+
+            Console.Write($"{matrix[i, j]} "); // Интерполяция строк
+
+        }
+        Console.WriteLine();
+    }
+}
