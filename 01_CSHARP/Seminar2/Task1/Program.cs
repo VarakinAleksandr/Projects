479cfaebc12db55224b1ae714f5d6f39f12aadcc
AleksandrVarakin <alexander.varakin@gmail.com>
2024-12-14 22:52:11 +0400

    Add Seminar2 Task1

---
+++ 01_CSHARP/Seminar2/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+Console.WriteLine("Введите первое число: ");
+int numberone =  Convert.ToInt32(Console.ReadLine());
+////Console.WriteLine("Введите второе число: ");
+//int numbertwo =  Convert.ToInt32(Console.ReadLine());
+if(numberone % 7 ==0 && numberone % 23 ==0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
